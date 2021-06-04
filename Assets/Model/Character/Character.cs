1c751608d5d7f27cf292ab0e61a7a3259e700948
MelnikovDaniil <3148dan@gmail.com>
2021-06-05 00:34:09 +0300

    Animation changed, added character script and base functionality

---
+++ Assets/Model/Character/Character.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float jumpForce;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 firstPressPos;
+    private Vector2 secondPressPos;
+    private Vector2 currentSwipe;
+
+    private bool isGrounded = true;
+    private bool isFalling = false;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = Input.mousePosition;
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            //swipe left
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+            {
+                Jump();
+            }
+            //swipe right
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+            {
+                Slip();
+            }
+        }
+
+        if (!isFalling && _rigidbody.velocity.y < 0)
+        {
+            isFalling = true;
+            _animator.SetTrigger("fall");
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            isFalling = false;
+            _animator.SetTrigger("jump");
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    
+    private void Slip()
+    {
+        _animator.SetTrigger("slip");
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.AddForce(Vector2.down * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _animator.SetBool("run", true);
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _animator.SetBool("run", false);
+        isGrounded = false;
+    }
+}
